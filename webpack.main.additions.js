61f96e37a8747834ee86484b3c3492240ad3d9f9
Vince Au <vinceau09@gmail.com>
2020-02-18 20:55:02 +1100

    Try to fix node notifier icon

---
+++ webpack.main.additions.js
@@ -0,0 +1,10 @@
+// Attempt to fix node-notifier icon
+// https://github.com/mikaelbr/node-notifier#within-electron-packaging
+
+module.exports = {
+    node: {
+      __filename: true,
+      __dirname: true
+    }
+}
+
